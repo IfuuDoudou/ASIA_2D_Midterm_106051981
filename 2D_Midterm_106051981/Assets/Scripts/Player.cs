932dd4e833596d15b25d0a0c1d800fd689b53fc8
Keon <55180628+keon981@users.noreply.github.com>
2020-11-17 10:47:53 +0800

    期中作業

---
+++ 2D_Midterm_106051981/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public GameObject BKB;
+    public Text textCount;
+    public int count;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.name == "死亡之台")
+        {
+            BKB.SetActive(true);
+        }
+
+
+        if (collision.tag == "吃貨")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "Key:" + count;
+        }
+      
+        if (collision.tag == "毒品")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
